524ce872e14332e0531e3c7f1a34672fde2905d6
PrinceTimilsina <142810341+PrinceTimilsina@users.noreply.github.com>
2024-08-15 21:06:26 +0545

    Add files via upload

---
+++ C++PERFECTNUM.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,nn,i,s=0;
+    cout<<"Enter a number:";
+    cin>>n;
+    nn=n;
+    for(i=1;i<=n/2;i++){
+    	if(n%i==0){
+    		s=s+i;
+		}
+	}
+	if(n==s){
+		cout<<"It is a PERFECT NUMBER";
+	}
+    else{
+    	cout<<"Sorry!It is not a PERFECT NUMBER,TRY AGAIN";
+	}
+	return 0;
+}
