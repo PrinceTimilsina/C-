a980d4a5ea720693507223de2abab34c6c57283d
PrinceTimilsina <142810341+PrinceTimilsina@users.noreply.github.com>
2024-11-26 19:30:46 +0545

    Update C++PERFECTNUM.cpp

--- C++PERFECTNUM.cpp
+++ C++PERFECTNUM.cpp
@@ -5,7 +5,7 @@ int main()
 int n,nn,i,s=0;      
     cout<<"Enter a number:";       
     cin>>n;                
-    nn=n;   
+    nn=n;              
     for(i=1;i<=n/2;i++){
     	if(n%i==0){
     		s=s+i;
